fb4de6862ee0a945a88771f2bbf6ba0a28c77bd4
zcemycl <yui.leung.15@ucl.ac.uk>
2021-03-30 16:37:24 +0100

    update

--- src/app.py
+++ src/app.py
@@ -12,7 +12,6 @@ def home():
 @app.route("/getdata",methods=['GET','POST'])
 def getdata():
     response =  jsonpify({'output':'Hello World','data':[10,2,8,5,6,1]})
-    response.headers.add('Access-Control-Allow-Origin','*')
     return response
 
 if __name__ == "__main__":
