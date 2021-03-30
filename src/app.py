f9279eb5de9a49da41f73101b22a3b8284a4727e
zcemycl <yui.leung.15@ucl.ac.uk>
2021-03-30 14:56:00 +0100

    update src/app.py

--- src/app.py
+++ src/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return jsonpify({'output':'Hello World'})
+    return jsonpify({'output':'Hello World','data':[10,2,8,5,6,1]})
 
 if __name__ == "__main__":
     app.run()
