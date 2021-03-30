cab3edeef2189b5811d3607fe33ad668d37a9f7b
zcemycl <yui.leung.15@ucl.ac.uk>
2021-03-30 16:15:38 +0100

    update

--- src/app.py
+++ src/app.py
@@ -3,7 +3,7 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/",methods=['POST'])
 def home():
     return jsonpify({'output':'Hello World','data':[10,2,8,5,6,1]})
 
