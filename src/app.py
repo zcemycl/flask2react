790a748caf6736218ea7e6543e0aec276a4f88d4
zcemycl <yui.leung.15@ucl.ac.uk>
2021-03-30 16:23:37 +0100

    fix access-control-allow-origin trial 1

--- src/app.py
+++ src/app.py
@@ -3,9 +3,15 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route("/",methods=['POST'])
+@app.route("/")
 def home():
     return jsonpify({'output':'Hello World','data':[10,2,8,5,6,1]})
+
+@app.route("/getdata",methods=['GET','POST'])
+def getdata():
+    response =  jsonpify({'output':'Hello World','data':[10,2,8,5,6,1]})
+    response.headers.add('Access-Control-Allow-Origin','*')
+    return response
 
 if __name__ == "__main__":
     app.run()
