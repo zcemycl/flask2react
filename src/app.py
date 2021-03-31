6046e108f3d3de856b3e70d8b12848c562f98489
zcemycl <yui.leung.15@ucl.ac.uk>
2021-04-01 00:59:10 +0100

    test socketio

--- src/app.py
+++ src/app.py
@@ -1,10 +1,12 @@
 from flask_jsonpify import jsonpify
-from flask import Flask, request, stream_with_context, Response
+from flask import Flask, request, stream_with_context, Response, render_template
 from flask_cors import CORS
 import random
+from flask_socketio import SocketIO
 
 app = Flask(__name__)
 CORS(app)
+socketio = SocketIO(app)
 
 
 @app.route("/")
@@ -17,6 +19,19 @@ def getdata():
     response = jsonpify({'output':'Hello Leo','data':data})
     return response
 
+@app.route("/session", methods=['GET', 'POST'])
+def sessions():
+    return render_template('session.html')
+
+def messageReceived(methods=['GET', 'POST']):
+    print('message was received!!!')
+
+@socketio.on('my event')
+def handle_my_custom_event(json, methods=['GET', 'POST']):
+    print('received my event: ' + str(json))
+    socketio.emit('my response', json, callback=messageReceived)
+
+
 if __name__ == "__main__":
-    app.run()
+    socketio.run(app)
 
